e319d8e6d2a02152f1d720e680b6d42918a57cfc
iwamoto <iwamoto@atob-net.co.jp>
2019-09-20 17:30:10 +0900

    2回目提出 あいさつの表示をログからtextView.textに変更。

--- app/src/main/java/jp/techacademy/taro/kirameki/aisatsuapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/taro/kirameki/aisatsuapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.app.TimePickerDialog
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -23,9 +24,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val timePickerDialog = TimePickerDialog(this,TimePickerDialog.OnTimeSetListener{
             view, hour,minute ->
             when(hour){
-                2, 3, 4, 5, 6, 7, 8, 9 -> Log.d("UI_PARTS","おはよう")
-                10,11,12,13,14,15,16,17 -> Log.d("UT_PARTS","こんにちは")
-                else -> Log.d("UT_PARTS","こんばんは")
+                2, 3, 4, 5, 6, 7, 8, 9 -> textView.text = "おはよう"
+                10,11,12,13,14,15,16,17 -> textView.text ="こんにちは"
+                else -> textView.text ="こんばんは"
             }
         },13,0,true)
         timePickerDialog.show()
